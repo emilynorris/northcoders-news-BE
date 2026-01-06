9cf785e7a0ac88e682267498856cff3a82a8a508
emilynorris <emilynorris2303@gmail.com>
2026-01-06 14:06:20 +0000

    Updated connection file for prod

--- db/connection.js
+++ db/connection.js
@@ -8,15 +8,13 @@ const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
-const config = {};
+const config = {}
 
 if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+  config.connectionString = process.env.DATABASE_URL
+  config.max = 2
 }
 
-module.exports = db (config)
+module.exports = db, new Pool(config)
